ffff8457a0241b9e799c0f9d31dac0444a9b3132
Patrick Schleizer <adrelanos@riseup.net>
2015-09-05 16:08:45 +0000

    Removed 'aptRemove chrony' since  https://github.com/marmarek/qubes-builder-debian/pull/18 was merged.

--- whonix-gateway/09_cleanup_post.sh
+++ whonix-gateway/09_cleanup_post.sh
@@ -12,9 +12,6 @@ source "${SCRIPTSDIR}/distribution.sh"
 debug ' Whonix post installation cleanup'
 ##### '-------------------------------------------------------------------------
 
-## Can be removed when https://github.com/marmarek/qubes-builder-debian/pull/18 was merged.
-aptRemove chrony
-
 ## Workaround. ntpdate needs to be removed here, because it can not be removed from
 ## template_debian/packages_qubes.list, because that would break minimal Debian templates.
 ## https://github.com/QubesOS/qubes-issues/issues/1102
