1d55da3a25cd997b15d5f4511ed508f566dc7f06
Patrick Schleizer <adrelanos@riseup.net>
2015-09-06 19:50:04 +0000

    fix

--- whonix-gateway/04_install_qubes_post.sh
+++ whonix-gateway/04_install_qubes_post.sh
@@ -184,12 +184,12 @@ debug ' Whonix post installation cleanup'
 ##### '-------------------------------------------------------------------------
 
 ## Can be removed when https://github.com/marmarek/qubes-builder-debian/pull/18 was merged.
-aptRemove chrony
+aptRemove chrony || true
 
 ## Workaround. ntpdate needs to be removed here, because it can not be removed from
 ## template_debian/packages_qubes.list, because that would break minimal Debian templates.
 ## https://github.com/QubesOS/qubes-issues/issues/1102
-aptRemove ntpdate
+aptRemove ntpdate || true
 DEBIAN_FRONTEND="noninteractive" DEBIAN_PRIORITY="critical" DEBCONF_NOWARNINGS="yes" \
         chroot $eatmydata_maybe apt-get ${APT_GET_OPTIONS} autoremove
 
