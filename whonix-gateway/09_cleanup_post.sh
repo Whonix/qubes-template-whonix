6299cc0076f44dd7c9bb878665244e53b325aec6
Patrick Schleizer <adrelanos@riseup.net>
2016-04-21 01:54:45 +0000

    cleanup, no longer need to delete these files as the related workarounds are no longer required

--- whonix-gateway/09_cleanup_post.sh
+++ whonix-gateway/09_cleanup_post.sh
@@ -11,12 +11,3 @@ source "${SCRIPTSDIR}/distribution.sh"
 ##### '-------------------------------------------------------------------------
 debug ' Whonix post installation cleanup'
 ##### '-------------------------------------------------------------------------
-
-#### '--------------------------------------------------------------------------
-info ' Removing files created during installation that are no longer required'
-#### '--------------------------------------------------------------------------
-rm -rf "${INSTALLDIR}/home.orig/user/Whonix"
-rm -rf "${INSTALLDIR}/home.orig/user/whonix_binary"
-rm -f "${INSTALLDIR}/etc/sudoers.d/whonix-build"
-rm -f "${INSTALLDIR}/etc/torbrowser.d/40_whonix_build"
-rm -f "${TMPDIR}/etc/sudoers.d/whonix-build"
